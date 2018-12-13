a2688e8c948851f536125131ab90d2a854f8c06c
Krishna Chaitanya M <chaitanya.jun12@gmail.com>
2018-12-13 23:47:35 +0530

    todo backbone app base code

---
+++ TodoApp/js/collections/todos.js
@@ -0,0 +1,22 @@
+var app = app | {};
+
+app.TodosList = Backbone.Collection.extend({
+    model: app.Todo,
+    localStorage: new Backbone.LocalStorage('todos-backbone'),
+
+    completed: function() {
+        return this.filter(function() {
+            return this.get('completed');
+        });
+    },
+
+    remaining: function() {
+        
+    },
+
+    comparator: function() {
+
+    }
+});
+
+app.Todos = new TodosList();
